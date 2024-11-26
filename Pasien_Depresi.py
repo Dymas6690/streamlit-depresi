379730488be752d9ebc7bc26f75c5c58dd0cd8ff
Dymas6690 <dymaspratama9h@gmail.com>
2024-11-26 18:56:42 +0700

    Update Pasien_Depresi.py

--- Pasien_Depresi.py
+++ Pasien_Depresi.py
@@ -4,19 +4,20 @@ import streamlit as st
 depresi_model = pickle.load(open('depresi_model.sav', 'rb'))
 
 st.title('Data Mining Prediksi Depresi')
+st.caption('Input angka 1-10')
 col1, col2 = st.columns(2)
 with col1 :
-    Age = st.text_input ('input nilai Age/Umur')
+    Age = st.text_input ('Input nilai Age/Umur')
 with col2 :
-    WorkPressure = st.text_input ('input nilai Work Pressure/Tekanan Kerja')
+    WorkPressure = st.text_input ('Input nilai Work Pressure/Tekanan Kerja')
 with col1 :
-    JobSatisfaction = st.text_input ('input nilai Job Satisfaction/Kepuasan Kerja')
+    JobSatisfaction = st.text_input ('Input nilai Job Satisfaction/Kepuasan Kerja')
 with col2 :
-    Haveyoueverhadsuicidalthoughts = st.text_input ('input nilai Have you ever had suicidal thoughts?/Pernahkah Anda memiliki pikiran untuk bunuh diri?')
+    Haveyoueverhadsuicidalthoughts = st.text_input ('Input nilai Have you ever had suicidal thoughts?/Pernahkah Anda memiliki pikiran untuk bunuh diri?')
 with col1 :
-    WorkHours = st.text_input ('input nilai Work Hours/Jam Kerja')
+    WorkHours = st.text_input ('Input nilai Work Hours/Jam Kerja')
 with col2 :
-    FinancialStress = st.text_input ('input nilai Financial Stress/Tekanan Keuangan')
+    FinancialStress = st.text_input ('Input nilai Financial Stress/Tekanan Keuangan')
 
 dep_diagnosis = ''
 
