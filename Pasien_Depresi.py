087d862f881052a98864e59371906fabf9792514
Dymas6690 <dymaspratama9h@gmail.com>
2024-11-27 22:09:42 +0700

    Update Pasien_Depresi.py

--- Pasien_Depresi.py
+++ Pasien_Depresi.py
@@ -8,24 +8,24 @@ st.title('Data Mining Prediksi Depresi')
 st.caption('Input angka 1-10')
 col1, col2 = st.columns(2)
 with col1 :
-    Age = st.text_input ('Input nilai Age/Umur')
+    Age = st.number_input ('Input nilai Age/Umur')
 with col2 :
-    WorkPressure = st.text_input ('Input nilai Work Pressure/Tekanan Kerja')
+    WorkPressure = st.slider ('Input nilai Work Pressure/Tekanan Kerja')
 with col1 :
-    JobSatisfaction = st.text_input ('Input nilai Job Satisfaction/Kepuasan Kerja')
+    JobSatisfaction = st.text_slider ('Input nilai Job Satisfaction/Kepuasan Kerja')
 with col2 :
-    Haveyoueverhadsuicidalthoughts = st.text_input ('Input nilai Have you ever had suicidal thoughts?/Pernahkah Anda memiliki pikiran untuk bunuh diri?')
+    Haveyoueverhadsuicidalthoughts = st.text_selectbox ('Input nilai Have you ever had suicidal thoughts?/Pernahkah Anda memiliki pikiran untuk bunuh diri?',['0','1'])
 with col1 :
-    WorkHours = st.text_input ('Input nilai Work Hours/Jam Kerja')
+    WorkHours = st.number_input ('Input nilai Work Hours/Jam Kerja')
 with col2 :
-    FinancialStress = st.text_input ('Input nilai Financial Stress/Tekanan Keuangan')
+    FinancialStress = st.slider ('Input nilai Financial Stress/Tekanan Keuangan')
 
 dep_diagnosis = ''
 
 if st.button('Test Prediksi Depresi'):
     prediction = depresi_model.predict([[Age, WorkPressure, JobSatisfaction, Haveyoueverhadsuicidalthoughts, WorkHours, FinancialStress]])
 
-    if(prediction[0] == 1):
+    if(prediction[0] == 0):
         dep_diagnosis = 'Pasien tidak Depresi'
     else :
         dep_diagnosis = 'Pasien Depresi'
